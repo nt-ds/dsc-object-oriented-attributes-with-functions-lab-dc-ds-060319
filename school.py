4bad6d356c6acc3b84e87862cd65080d6a30b6b8
Ngoc <ntran19@jhu.edu>
2019-06-29 13:09:52 -0400

    changes by Ngoc

--- school.py
+++ school.py
@@ -1,1 +1,20 @@
 class School:
+    def __init__(self, name):
+        self.name = name
+        self.roster = {}
+        
+    def add_student(self, full_name, grade):
+        if grade not in self.roster:
+            self.roster[grade] = []
+            self.roster[grade].append(full_name)
+        else:
+            self.roster[grade].append(full_name)
+    
+    def grade(self, grade):
+        return self.roster[grade]
+    
+    def sort_roster(self):
+        new_dict = self.roster
+        for key in new_dict:
+            new_dict[key].sort()
+        return new_dict
